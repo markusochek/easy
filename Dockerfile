9730ab37902c5e793260fba6170bdb1db859d56e
Марк Сагалаев <sagalaev.bot@gmail.com>
2024-04-04 15:51:08 +0400

    3

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM node:16-slim
 
 WORKDIR /app
-RUN npm install express
+RUN npm i express
 RUN npm i easy-yandex-s3@1.1.8
 RUN npm i express-fileupload
+RUN npm i aws-sdk
 
 COPY ./index.js .
 
--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
 const EasyYandexS3 = require("easy-yandex-s3");
+const expressFileUpload = require('express-fileupload');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(expressFileUpload());
 
 const s3 = new EasyYandexS3({
     auth: {
@@ -9,12 +17,95 @@ const s3 = new EasyYandexS3({
     debug: false
 });
 
-module.exports.handler = async function (event, context) {
-    const upload = await s3.Upload({buffer: context.files.photo.data}, "/gaika/");
-    console.log(upload)
+const AWS = require('aws-sdk');
 
-    return {
-        statusCode: 200,
-        body: upload.key
-    };
+const mq = new AWS.SQS({
+    'region': 'ru-central1',
+    'endpoint': 'https://message-queue.api.cloud.yandex.net',
+});
+
+async function createQueue() {
+    params = {
+        'QueueName': 'mq_example_nodejs_sdk',
+    }
+
+    result = await mq.createQueue(params).promise();
+    queueUrl = result['QueueUrl'];
+
+    console.log('Queue created, URL: ' + queueUrl);
+
+    return queueUrl;
 }
+
+async function sendMessage(queueUrl) {
+    params = {
+        'QueueUrl': queueUrl,
+        'MessageBody': 'test message',
+    }
+
+    result = await mq.sendMessage(params).promise();
+
+    console.log('Message sent, ID: ' + result['MessageId']);
+}
+
+async function receiveMessage() {
+    params = {
+        'QueueUrl': queueUrl,
+        'WaitTimeSeconds': 10,
+    }
+
+    result = await mq.receiveMessage(params).promise();
+
+    result['Messages'].forEach(async function(msg) {
+        console.log('Message received')
+        console.log('ID: ' + msg['MessageId'])
+        console.log('Body: ' + msg['Body'])
+
+        deleteParams = {
+            'QueueUrl': queueUrl,
+            'ReceiptHandle': msg['ReceiptHandle'],
+        }
+
+        await mq.deleteMessage(deleteParams).promise()
+    })
+}
+
+async function deleteQueue() {
+    params = {
+        'QueueUrl': queueUrl,
+    }
+
+    result = await mq.deleteQueue(params).promise();
+
+    console.log('Queue deleted')
+}
+
+app.post("/addImage", async function (request, response) {
+    // const upload = await s3.Upload({buffer: request.files.photo.data}, "/gaika/");
+    params = {
+        'QueueName': 'mq_example_nodejs_sdk',
+    }
+
+    result = await mq.createQueue(params).promise();
+    queueUrl = result['QueueUrl'];
+
+    console.log('Queue created, URL: ' + queueUrl);
+    // queueUrl = await createQueue();
+    // await sendMessage(queueUrl);
+    // await receiveMessage(queueUrl);
+    // await deleteQueue(queueUrl);
+
+    response.send(
+        {
+            'statusCode': 200,
+            'body': "fwefwfewfweewewfefw"
+        }
+    )
+});
+
+
+app.listen(3000);
+// app.listen(process.env.PORT, () => {
+//     console.log(`App listening at port ${process.env.PORT}`);
+// });
+
